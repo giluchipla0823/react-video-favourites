ebb01cface197d256f397a649c1838080ab8bf8b
Gino Luiggi Chirinos Plasencia <luiggiplasencia0823@gmail.com>
2019-08-06 19:28:10 +0200

    build de la aplicación

---
+++ build/precache-manifest.3d24c62411013ad43573888b64a947db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbbcba0d0f57fdd01c1242563ea292a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba0909774e4e06c1325a",
+    "url": "/static/css/main.439404a0.chunk.css"
+  },
+  {
+    "revision": "6eee531f70afaa85fc2b",
+    "url": "/static/js/2.c333251b.chunk.js"
+  },
+  {
+    "revision": "ba0909774e4e06c1325a",
+    "url": "/static/js/main.1f66c25f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
